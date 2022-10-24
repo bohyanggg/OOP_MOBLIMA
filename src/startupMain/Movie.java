0d3ade4ee38966939dd9a5b2121ea602794c0864
ZHLOW <100467835+ZHLOW@users.noreply.github.com>
2022-10-24 15:08:50 +0800

    movie and config

--- src/startupMain/Movie.java
+++ src/startupMain/Movie.java
@@ -1,72 +1,119 @@
 package startupMain;
 
+import java.util.Scanner;
+
 public class Movie {
 
 	private String title;
-	private String showingStatus;
+	private int showingStatus;
 	private String synopsis;
 	private String director;
 	private String[] cast;
-	private double overallReviewerRating;
-	//private smth collectionOfReviewsAndRatings
-	//add collection of past reviews and ratings
-
-	public Movie(String title, String synopsis, String director, String[] cast) {
-		this.title = title;
-		showingStatus = "Coming Soon";
-		this.synopsis = synopsis;
-		this.director = director;
-		this.cast = cast;
-		overallReviewerRating = 0;
-		//add collection of past reviews and ratings
-	}
+	private double overallReviewerRating; //1 to 5
+	private String[] reviewsAndRatings; //stored as (String review, double rating) as one array element
+	private int ticketSales;
+	private Scanner sc = new Scanner(System.in);
+	
+//	public Movie() {
+//		title = "";
+//		showingStatus = 0; // 0 = Coming Soon
+//		synopsis = "";
+//		director = "";
+//		cast = [];
+//		overallReviewerRating = 0;
+//		ticketSales = 0;
+//	}
 
 	public String getTitle() {
 		return title;
 	}
 
-	public void setTitle(String title) {
-		this.title = title;
+	public void setTitle() {
+		System.out.print("Enter Movie Title: ");
+		String newTitle = sc.nextLine();
+		title = newTitle;
 	}
 
 	public String getShowingStatus() {
-		return showingStatus;
+		switch(showingStatus) {
+			case 0: return "Coming Soon";
+			case 1: return "Preview";
+			case 2: return "Now Showing";
+			case 3: return "End of Showing";
+		}
+		return "Error";
 	}
 
-	public void setShowingStatus(String showingStatus) {
-		this.showingStatus = showingStatus;
+	//ADD 4 OPTIONS TO CHOOSE (0=Coming Soon, 1=Preview, 2=Now Showing, 3=End of Showing)
+	public void setShowingStatus() {
+		System.out.println("Enter Showing Status:\n"
+						  + "0: Coming Soon\n"
+						  + "1: Preview\n"
+						  + "2: Now Showing\n"
+						  + "3: End of Showing\n");
+		int newShowingStatus = sc.nextInt();
+		showingStatus = newShowingStatus;
 	}
 
 	public String getSynopsis() {
 		return synopsis;
 	}
 
-	public void setSynopsis(String synopsis) {
-		this.synopsis = synopsis;
+	public void setSynopsis() {
+		System.out.print("Enter Synopsis: ");
+		String newSynopsis = sc.nextLine();
+		synopsis = newSynopsis;
 	}
 
 	public String getDirector() {
 		return director;
 	}
 
-	public void setDirector(String director) {
-		this.director = director;
+	public void setDirector() {
+		System.out.print("Enter Director: ");
+		String newDirector = sc.nextLine();
+		director = newDirector;
 	}
 
 	public String[] getCast() {
 		return cast;
 	}
 
-	public void setCast(String[] cast) {
-		this.cast = cast;
+	public void setCast() {
+		System.out.print("Enter No. of Cast Members: "); //at least 2
+		int numCast = sc.nextInt();
+		String[] newCast = new String[numCast];
+		for (int i=0; i<numCast; i++) {
+			System.out.print("Enter Cast Member " + i + " : ");
+			newCast[i] = sc.nextLine();
+		}
+		cast = newCast;
 	}
 
+	//DISPLAY "NA" IF NO RATINGS
 	public double getOverallReviewerRating() {
 		return overallReviewerRating;
 	}
 
 	//CALCULATE USING AVERAGE OF ALL PAST RATINGS
-//	public void setOverallReviewerRating(double overallReviewerRating) {
-//		this.overallReviewerRating = overallReviewerRating;
-//	}
+	public void setOverallReviewerRating(double overallReviewerRating) {
+		this.overallReviewerRating = overallReviewerRating;
+	}
+
+	public String[] getReviewsAndRatings() {
+		return reviewsAndRatings;
+	}
+
+	public void setReviewsAndRatings(String[] reviewsAndRatings) {
+		this.reviewsAndRatings = reviewsAndRatings;
+	}
+
+	public int getTicketSales() {
+		return ticketSales;
+	}
+
+	public void setTicketSales(int ticketSales) {
+		this.ticketSales = ticketSales;
+	}
 }
+
--- src/startupMain/MovieListingConfig.java
+++ src/startupMain/MovieListingConfig.java
@@ -18,8 +18,8 @@ public class MovieListingConfig extends MovieListingDisplay {
 			choice = sc.nextInt();
 			
 			switch (choice) {
-//				case 1: createMovieListing();
-//						break;
+				case 1: createMovieListing();
+						break;
 //				case 2: updateMovieListing();
 //						break;
 //				case 3: removeMovieListing();
@@ -32,21 +32,8 @@ public class MovieListingConfig extends MovieListingDisplay {
 	}
 	
 	public void createMovieListing() {
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter Movie Title: ");
-		String title = sc.nextLine();
-		System.out.print("Enter Synopsis: ");
-		String synopsis = sc.nextLine();
-		System.out.print("Enter Director: ");
-		String director = sc.nextLine();
-		System.out.print("Enter No. of Cast Members: "); //at least 2
-		int numCast = sc.nextInt();
-		String[] cast = new String[numCast];
-		for (int i=0; i<numCast; i++) {
-			System.out.print("Enter Cast Member " + i + " : ");
-			cast[i] = sc.nextLine();
-		}
-		Movie newMovie = new Movie(title, synopsis, director, cast);
+		Movie newMovie = new Movie();
+
 		//add newMovie to movie list
 	}
 }
