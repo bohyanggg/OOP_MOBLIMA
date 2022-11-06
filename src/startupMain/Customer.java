ecfbd68c28b3d8c375cad0eb8eb94be3f0784e0a
ZHLOW <100467835+ZHLOW@users.noreply.github.com>
2022-11-06 22:32:40 +0800

    customer

--- src/startupMain/Customer.java
+++ src/startupMain/Customer.java
@@ -16,106 +16,107 @@ public class Customer {
 
 	public void showCustomerSystem() {
 		// TODO Auto-generated method stub
-		System.out.println("1. Search/List movie");
-		System.out.println("2. Check seat availibility and selection of seat(s)");
-		System.out.println("3. View booking history");
-		System.out.println("4. List Top 5 ranking of movies");
-		System.out.println("5. Quit");
-		System.out.print("Enter your choice: ");
-		Scanner Choice = new Scanner(System.in);
-		int userChoice = Choice.nextInt();
-		while(userChoice != 5) {
-			if (userChoice == 1) { //i copied over this whole part from MovieListingConfig 's viewmovielisting
-				ArrayList<Movie> movieList = new ArrayList<>(); //Create arraylist to store movie objects from txt
-				movieList = ResourceManager.getmovieList(movieList); //to take objects from txt and store into movieList array
-				int i = 1; //To number the movies
-				System.out.print("\n");
-				for (Movie movie: movieList)
-				{
-					System.out.print("Movie " + i + ": ");
-					i++;
-					movie.viewimportantMovieDetails();
+		while (true) {
+			System.out.println("1. Search/List movie");
+			System.out.println("2. Check seat availibility and selection of seat(s)");
+			System.out.println("3. View booking history");
+			System.out.println("4. List Top 5 ranking of movies");
+			System.out.println("5. Quit");
+			System.out.print("Enter your choice: ");
+			Scanner Choice = new Scanner(System.in);
+			int userChoice = Choice.nextInt();
+			while(userChoice != 5) {
+				if (userChoice == 1) { //i copied over this whole part from MovieListingConfig 's viewmovielisting
+					ArrayList<Movie> movieList = new ArrayList<>(); //Create arraylist to store movie objects from txt
+					movieList = ResourceManager.getmovieList(movieList); //to take objects from txt and store into movieList array
+					int i = 1; //To number the movies
 					System.out.print("\n");
-				}
-				int totalMovie = i -1;
-				i = 1;
-				System.out.println("\nView all details of a Movie? (Y/N)");
-				Scanner sc = new Scanner(System.in);
-				String choice = sc.next();
-				if (choice.equalsIgnoreCase("Y"))
-				{
-					System.out.println("\nEnter Movie number: ");
-					int movieNumber = sc.nextInt();
-					if (movieNumber > totalMovie)
-					{
-						System.out.println("\nInvalid Input Number!");
-						return;
-					}
 					for (Movie movie: movieList)
 					{
-						if (i == movieNumber)
-						{
-							movie.viewallMovieDetails();
-							break;
-						}
+						System.out.print("Movie " + i + ": ");
 						i++;
+						movie.viewimportantMovieDetails();
+						System.out.print("\n");
 					}
-				}
-				else if (choice.equalsIgnoreCase("N"))
-				{
-					break;
-				}
-				else
-				{
-					System.out.println("\nInvalid Input!");
-					break;
-				}
-				
-			}
-			if (userChoice == 2) {
-				Booking booking = new Booking();
-				Cineplex cineplex = new Cineplex();
-				// movieDetails.GetMovieDetailsFromFile();
-				booking.CineplexChoiceDisplay(cineplex);
-				booking.ChooseACineplex();
-				String cineplexChoice = booking.getCineplexChoice();
-				if (cineplexChoice == "Cathay Cineplexes Lot One") {
-					ArrayList<String> cinemas = cineplex.GetCinemasOfCineplex(cineplexChoice);
-					booking.CinemaChoiceDisplay(cineplex, cinemas);
-					booking.ChooseACinema();
-					if (booking.getCinemaChoice() == "Quit"){
-						continue;
+					int totalMovie = i -1;
+					i = 1;
+					System.out.println("\nView all details of a Movie? (Y/N)");
+					Scanner sc = new Scanner(System.in);
+					String choice = sc.next();
+					if (choice.equalsIgnoreCase("Y"))
+					{
+						System.out.println("\nEnter Movie number: ");
+						int movieNumber = sc.nextInt();
+						if (movieNumber > totalMovie)
+						{
+							System.out.println("\nInvalid Input Number!");
+							return;
+						}
+						for (Movie movie: movieList)
+						{
+							if (i == movieNumber)
+							{
+								movie.viewallMovieDetails();
+								break;
+							}
+							i++;
+						}
 					}
-					Cinema lotOne = new Cinema(booking.getCinemaChoice());
-					ArrayList<String> movies = lotOne.GetMoviesOfCinema();
-					booking.MovieChoiceDisplay(lotOne, movies);
-					booking.ChooseAMovie();
-					if (booking.getMovieChoice() == "Quit"){
-						continue;
+					else if (choice.equalsIgnoreCase("N"))
+					{
+						break;
 					}
-					String movieChoice = booking.getMovieChoice();
-					movieDetails.DisplayMovieDetails(movieChoice);
-					booking.ChooseASeat();
-					movieDetails.UpdateBookedSeats(movieChoice, booking.getSeatChoice());
+					else
+					{
+						System.out.println("\nInvalid Input!");
+						break;
+					}
+					
 				}
-				else if (cineplexChoice == "Cathay Cineplexes Clementi Mall") {
-					Cinema clementiMall = new Cinema(cineplexChoice);
-					clementiMall.Display();
-				}
-				else if (cineplexChoice == "Cathay Cineplexes Jewel") {
-					Cinema jewel = new Cinema(cineplexChoice);
-					jewel.Display();
-				}
-				else if (cineplexChoice == "Quit"){
-					userChoice = 5;
-					showCustomerSystem();
-				}
-				else {
-					userChoice = 5;
-					showCustomerSystem();
+				if (userChoice == 2) {
+					Booking booking = new Booking();
+					Cineplex cineplex = new Cineplex();
+					// movieDetails.GetMovieDetailsFromFile();
+					booking.CineplexChoiceDisplay(cineplex);
+					booking.ChooseACineplex();
+					String cineplexChoice = booking.getCineplexChoice();
+					if (cineplexChoice == "Cathay Cineplexes Lot One") {
+						ArrayList<String> cinemas = cineplex.GetCinemasOfCineplex(cineplexChoice);
+						booking.CinemaChoiceDisplay(cineplex, cinemas);
+						booking.ChooseACinema();
+						if (booking.getCinemaChoice() == "Quit"){
+							continue;
+						}
+						Cinema lotOne = new Cinema(booking.getCinemaChoice());
+						ArrayList<String> movies = lotOne.GetMoviesOfCinema();
+						booking.MovieChoiceDisplay(lotOne, movies);
+						booking.ChooseAMovie();
+						if (booking.getMovieChoice() == "Quit"){
+							continue;
+						}
+						String movieChoice = booking.getMovieChoice();
+						movieDetails.DisplayMovieDetails(movieChoice);
+						booking.ChooseASeat();
+						movieDetails.UpdateBookedSeats(movieChoice, booking.getSeatChoice());
+					}
+					else if (cineplexChoice == "Cathay Cineplexes Clementi Mall") {
+						Cinema clementiMall = new Cinema(cineplexChoice);
+						clementiMall.Display();
+					}
+					else if (cineplexChoice == "Cathay Cineplexes Jewel") {
+						Cinema jewel = new Cinema(cineplexChoice);
+						jewel.Display();
+					}
+					else if (cineplexChoice == "Quit"){
+						userChoice = 5;
+						showCustomerSystem();
+					}
+					else {
+						userChoice = 5;
+						showCustomerSystem();
+					}
 				}
 			}
 		}
 	}
-
 }
